44308f7be121bcbd5e4d3a47510ab006b6b25bf1
Anton Fejer <anton.28@hotmail.com>
2021-07-04 16:54:24 -0300

    Create ListOfGifs and Gif components

---
+++ src/components/ListOfGifs/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import getGifs from '../../services/getGifs';
+import Gif from '../Gif';
+
+export default function ListOfGifs({ params }) {
+  const { keyword } = params
+  const [loading, setLoading] = useState(false)
+  const [gifs, setGifs] = useState([])
+
+  useEffect(() => {
+    setLoading(true)
+    getGifs({ keyword }).then(gifs => {
+      setGifs(gifs)
+      setLoading(false)
+    })
+  }, [keyword])
+
+  if (loading) return <p>CARGANDO ...</p>
+
+  return (
+    <div>
+      {
+        gifs.map(({id, title, url}) => (
+          <Gif 
+            key={id} 
+            title={title} 
+            url={url} 
+          />
+      ))}
+    </div>
+  )
+}
