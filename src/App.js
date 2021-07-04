733548f9cb1af55f0c1cfb1c5be5225060b52b93
Anton Fejer <anton.28@hotmail.com>
2021-07-04 16:53:19 -0300

    Add wouter and update App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Link, Route } from 'wouter'
 import './App.css';
+import ListOfGifs from './components/ListOfGifs';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <Link to='/gif/panda'>Gifs de Pandas</Link>
+        <Link to='/gif/naruto'>Gifs de Naruto</Link>
+        <Link to='/gif/garfield'>Gifs de Garfield</Link>
+        <Route
+          component={ListOfGifs}
+          path='/gif/:keyword'
+        />
+      </section>
     </div>
   );
 }
